40819b1d880090d01e7b0f79faf8953123a38983
Ruth <ruth.vargas@srlpunto.com>
2018-06-15 07:41:06 -0400

    test print digit 0 to 9

--- tdd/int2lcd/src/test/java/edu.alsie.tdd.int2lcd/Int2lcdTest.java
+++ tdd/int2lcd/src/test/java/edu.alsie.tdd.int2lcd/Int2lcdTest.java
@@ -68,7 +68,83 @@ public class Int2lcdTest {
   public void testGiven21ThenPrintDigit() {
     Int2lcd int2lcd = new Int2lcd();
     String expected = " \n" + "  |\n" +"  |\n";
+    String actual = int2lcd.printDigit(21);
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testGiven1ThenPrintDigit() {
+    Int2lcd int2lcd = new Int2lcd();
+    String expected = " \n" + "  |\n" +"  |\n";
     String actual = int2lcd.printDigit(1);
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testGiven2ThenPrintDigit() {
+    Int2lcd int2lcd = new Int2lcd();
+    String expected = " _ \n" + " _|\n" + "|_ \n";;
+    String actual = int2lcd.printDigit(2);
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testGiven3ThenPrintDigit() {
+    Int2lcd int2lcd = new Int2lcd();
+    String expected = " _ \n" + " _|\n" + " _|\n";
+    String actual = int2lcd.printDigit(3);
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testGiven4ThenPrintDigit() {
+    Int2lcd int2lcd = new Int2lcd();
+    String expected = " \n" + "|_|\n" + "  |\n";
+    String actual = int2lcd.printDigit(4);
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testGiven5ThenPrintDigit() {
+    Int2lcd int2lcd = new Int2lcd();
+    String expected = " _ \n" + "|_ \n" + " _|\n";
+    String actual = int2lcd.printDigit(5);
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testGiven6ThenPrintDigit() {
+    Int2lcd int2lcd = new Int2lcd();
+    String expected = " _ \n" + "|_ \n" + "|_|\n";
+    String actual = int2lcd.printDigit(6);
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void testGiven7ThenPrintDigit() {
+    Int2lcd int2lcd = new Int2lcd();
+    String expected = " _ \n" + "  |\n" +"  |\n";
+    String actual = int2lcd.printDigit(7);
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void testGiven8ThenPrintDigit() {
+    Int2lcd int2lcd = new Int2lcd();
+    String expected = " _ \n" + "|_|\n" + "|_|\n";
+    String actual = int2lcd.printDigit(8);
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void testGiven9ThenPrintDigit() {
+    Int2lcd int2lcd = new Int2lcd();
+    String expected = " _ \n" + "|_|\n" + " _|\n";
+    String actual = int2lcd.printDigit(9);
+    Assert.assertEquals(expected, actual);
+  }
+  @Test
+  public void testGiven0ThenPrintDigit() {
+    Int2lcd int2lcd = new Int2lcd();
+    String expected = " _ \n" + "| |\n" + "|_|\n";
+    String actual = int2lcd.printDigit(0);
     Assert.assertEquals(expected, actual);
   }
 

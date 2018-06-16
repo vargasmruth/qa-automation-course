ac08286170193aa425cfbd4258b0c21ab7d4dcf8
Ruth <ruth.vargas@srlpunto.com>
2018-06-15 22:39:29 -0400

    print int to lcd for the client

--- tdd/int2lcd/src/main/java/edu.alsie.tdd.int2lcd/Main.java
+++ tdd/int2lcd/src/main/java/edu.alsie.tdd.int2lcd/Main.java
@@ -4,4 +4,10 @@ package edu.alsie.tdd.int2lcd;
  * Created by Ruth on 14/6/2018.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        Int2lcd int2lcd = new Int2lcd();
+        System.out.println(int2lcd.printIntToLCD(123456789));
+     }
+
 }
